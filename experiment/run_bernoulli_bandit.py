6d277405759874748f7fd2af51641bdfa62b4935
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2020-03-21 11:37:31 +0100

    Add GIRO algorithm, add LinPHE algorithm

--- experiment/run_bernoulli_bandit.py
+++ experiment/run_bernoulli_bandit.py
@@ -16,12 +16,16 @@ from environments.BernoulliDiscreteBanditEnv import BernoulliDiscreteBanditEnv
 from utils.stats.BernoulliDistribution import BernoulliDistribution
 from bandit.discrete.TSBanditBernoulli import TSBanditBernoulli
 from bandit.discrete.UCB1Bandit import UCB1Bandit
+from bandit.discrete.GIROBernoulliBandit import GIROBernoulliBandit
+from bandit.discrete.LinPHEBernoulli import LinPHEBernoulli
 
-N_ARMS = 2
-ARMS_PROBABILITIES_PARAMETERS = [0.8, 0.1]
+
+N_ARMS = 5
+ARMS_PROBABILITIES_PARAMETERS = [0.4, 0.5, 0.7, 0.8, 0.9]
+PRICE_LIST = [50, 40, 30, 20, 10]
 N_ROUNDS = 1000
 BASIC_OUTPUT_FOLDER = "../report/bernoulli_bandit/"
-BANDIT_NAMES = ['TS']
+BANDIT_NAMES = ['TS', 'UCB1', 'GIRO', 'LINPHE']
 
 
 def get_arguments():
@@ -38,20 +42,37 @@ def get_arguments():
                                                                                    "to store the output",
                         type=str)
     parser.add_argument("-b", "--bandit_name", help="Name of the bandit to be used in the experiment")
+    parser.add_argument("-a", "--perturbation_hp", help="Parameter for perturbing the history", type=float, default=0.0)
+    parser.add_argument("-l", "--regularization", help="Regularization parameter", type=float, default=0.0)
+    parser.add_argument("-s", "--save_result", help="Whether to store results or not", type=lambda x: int(x) != 0,
+                        default=0)
     return parser.parse_args()
 
 
-def get_bandit(bandit_name: str) -> DiscreteBandit:
+def get_bandit(args) -> DiscreteBandit:
     """
     Retrieve the bandit to be used in the experiment according to the bandit name
 
-    :param bandit_name: name of the bandit to be used
+    :param args: command line arguments
     :return: bandit that will be used to carry out the experiment
     """
+    bandit_name = args.bandit_name
+
     if bandit_name == "TS":
         bandit = TSBanditBernoulli(n_arms=N_ARMS)
     elif bandit_name == "UCB1":
         bandit = UCB1Bandit(n_arms=N_ARMS)
+    elif bandit_name == "GIRO":
+        bandit = GIROBernoulliBandit(n_arms=N_ARMS, a=args.perturbation_hp)
+    elif bandit_name == "LINPHE":
+        features = np.zeros(shape=(N_ARMS, 2))
+        for i in range(N_ARMS):
+            features[i, 0] = PRICE_LIST[i]
+            features[i, 1] = 1
+
+        bandit = LinPHEBernoulli(n_arms=N_ARMS, perturbation=args.perturbation_hp,
+                                 regularization=args.regularization,
+                                 features=features, features_dim=2)
     else:
         raise argparse.ArgumentError("The name of the bandit to be used is not in the available ones")
 
@@ -66,17 +87,17 @@ def main(args):
 
     env = BernoulliDiscreteBanditEnv(n_arms=N_ARMS, probabilities=probabilities)
 
-    bandit = get_bandit(args.bandit_name)
+    bandit = get_bandit(args=args)
 
     for t in range(0, args.n_rounds):
         # Choose arm
-        idx_ts = bandit.pull_arm()
+        idx = bandit.pull_arm()
 
         # Observe reward
-        reward_ts = env.round(pulled_arm=idx_ts)
+        reward = env.round(pulled_arm=idx)
 
         # Update bandit
-        bandit.update(pulled_arm=idx_ts, reward=reward_ts)
+        bandit.update(pulled_arm=idx, reward=reward)
 
     return bandit.collected_rewards
 
@@ -108,17 +129,18 @@ else:
     results = Parallel(n_jobs=args.n_jobs, backend='loky')(
         delayed(run)(id=id, seed=seed, args=args) for id, seed in zip(range(args.n_runs), seeds))
 
-# Set up writing folder and file
-fd, folder_path_with_date = handle_folder_creation(result_path=args.output_folder)
+if args.save_result:
+    # Set up writing folder and file
+    fd, folder_path_with_date = handle_folder_creation(result_path=args.output_folder)
 
-# Writing results and experiment details
-with open("{}reward_{}.pkl".format(folder_path_with_date, args.bandit_name), "wb") as output:
-    pickle.dump(results, output)
+    # Writing results and experiment details
+    with open("{}reward_{}.pkl".format(folder_path_with_date, args.bandit_name), "wb") as output:
+        pickle.dump(results, output)
 
-fd.write("Bandit experiment\n")
-fd.write("Number of arms: {}\n".format(N_ARMS))
-fd.write("Probability distributions: {}\n".format(ARMS_PROBABILITIES_PARAMETERS))
-fd.write("Number of runs: {}\n".format(args.n_runs))
-fd.write("Horizon: {}\n".format(args.n_rounds))
-fd.write("Bandit algorithm: {}\n".format(args.bandit_name))
-fd.close()
+    fd.write("Bandit experiment\n")
+    fd.write("Number of arms: {}\n".format(N_ARMS))
+    fd.write("Probability distributions: {}\n".format(ARMS_PROBABILITIES_PARAMETERS))
+    fd.write("Number of runs: {}\n".format(args.n_runs))
+    fd.write("Horizon: {}\n".format(args.n_rounds))
+    fd.write("Bandit algorithm: {}\n".format(args.bandit_name))
+    fd.close()
